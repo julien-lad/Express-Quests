7f9e75aa29eb58d5a8f6446cd7b50df228fc2526
julien-lad <julien.ladreyt@gmail.com>
2023-06-10 16:14:09 +0200

    express 6 terminé

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,32 +1,5 @@
 const database = require("./database");
 
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    colors: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    colors: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
-
 const getMovies = (req, res) => {
   let sql = "SELECT * FROM movies";
   const sqlValues = [];
@@ -71,7 +44,6 @@ const getMovieById = (req, res) => {
       console.error(err);
       res.status(500).send("Error retrieving data from database")
     });
-  const movie = movies.find((movie) => movie.id === id);
 };
 
 const postMovie = (req, res) => {
