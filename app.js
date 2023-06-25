1da5b5ea8cf6a0cdfc82cd7bb38a75afa2f38f2c
ladreyt <julien.ladreyt@gmail.com>
2023-06-25 12:23:22 +0200

    paramétrage de argon 2- WIP

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require ("dotenv").config();
 const { validateMovie, validateUser } = require("./validators.js");
-
+const { hashPassword } = require ("./auth.js");
+const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 const express = require("express");
 
 const app = express();
@@ -8,23 +10,6 @@ const app = express();
 app.use(express.json());
 
 const port = process.env.APP_PORT ?? 6008;
-
-const welcome = (req, res) => {
-  console.log("page d'accueil")
-  res.send("Welcome to my favourite movie list");
-};
-
-app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
-
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-
-const userHandlers = require("./userHandlers");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
 
 app.listen(port, (err) => {
   if (err) {
@@ -34,11 +19,24 @@ app.listen(port, (err) => {
   }
 });
 
+const welcome = (req, res) => {
+  console.log("page d'accueil")
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
 app.post("/api/movies", validateMovie, movieHandlers.postMovie);
-app.post("/api/users", validateUser, userHandlers.postUser);
+app.post("/api/users", hashPassword, validateUser, userHandlers.postUser);
 
 app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
-app.put("/api/users/:id", validateUser, userHandlers.updateUser);
+app.put("/api/users/:id", hashPassword, validateUser, userHandlers.updateUser);
 
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.delete("/api/users/:id", movieHandlers.deleteUser);
+
+
