4b18b68b26b60836981e2ab2d0e236f9afc1df37
ladreyt <julien.ladreyt@gmail.com>
2023-06-28 08:33:22 +0200

    express_08 wip mur authentification

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require ("dotenv").config();
 const { validateMovie, validateUser } = require("./validators.js");
-const { hashPassword } = require ("./auth.js");
+const { hashPassword, verifyPassword, verifyToken } = require ("./auth.js");
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
 const express = require("express");
@@ -24,13 +24,21 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
+
+
+app.post(
+  "/api/login", 
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+  );
+
 app.get("/", welcome);
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
-app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.post("/api/movies", verifyToken, validateMovie, movieHandlers.postMovie);
 app.post("/api/users", hashPassword, validateUser, userHandlers.postUser);
 
 app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
